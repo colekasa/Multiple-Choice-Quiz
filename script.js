46eb67b10be35296d3c88d5afd7bd5ef2bd8c28a
Cole <ce.kasabian@gmail.com>
2023-01-21 23:05:57 -0800

    viewYourScore function added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var startBtn = document.getElementById('start-btn')
-var scoreBtn = document.getElementById('highScore')
-var timerEl = document.getElementById('timer')
-var answerEl = document.getElementById('determination')
+var startBtn = document.getElementById('start-btn');
+var scoreBtn = document.getElementById('highScore');
+var timerEl = document.getElementById('timer');
+var answerEl = document.getElementById('determination');
 var timeLeft = 6;
 var timeInterval;
+var score = 0;
 var questions = [
     {
         text:'Question 1',
@@ -65,64 +66,68 @@ var questions = [
         choices: ['A','B','C','D'],
         correct: 'A',
     },
-]
+];
 
 var Q = 0
 function askQuestion(){
     document.querySelector('.question').textContent = questions[Q].text
-    var choicesUl = document.querySelector('.choices')
-    choicesUl.innerHTML = ''
+    var choicesUl = document.querySelector('.choices');
+    choicesUl.innerHTML = '';
     questions[Q].choices.forEach(function(choice){
-        console.log(choice)
-        var liEl = document.createElement('button')
-        liEl.textContent = choice
-        liEl.setAttribute('value', choice)
+        console.log(choice);
+        var liEl = document.createElement('button');
+        liEl.textContent = choice;
+        liEl.setAttribute('value', choice);
         liEl.onclick = function(event){
-            var selected = event.target.value
+            var selected = event.target.value;
             if(selected===questions[Q].correct){
-                document.querySelector('.determination').textContent = 'Correct!'
-                localStorage.setItem("correct",12)
-                localStorage.getItem("correct")
+                document.querySelector('.determination').textContent = 'Correct!';
+                localStorage.setItem("correct",score++*12);
+                localStorage.getItem("correct");
             }
             else{
-                document.querySelector('.determination').textContent = 'Incorrect!'
-                localStorage.setItem("incorrect",0)
-                localStorage.getItem("incorrect")
+                document.querySelector('.determination').textContent = 'Incorrect!';
+                localStorage.setItem("incorrect",score);
+                localStorage.getItem("incorrect");
             }
             Q++;
             if(Q===questions.length){
-                endGame()
+                endGame();
+                viewYourScore();
             }
             else{
-                askQuestion()
+                askQuestion();
             }
         }
-        choicesUl.appendChild(liEl)
+        choicesUl.appendChild(liEl);
     })
     
 }
 function endGame(){ 
-        document.querySelector('.quiz').classList.add('hide')
+        document.querySelector('.quiz').classList.add('hide');
 }
 
 function viewYourScore(){
-   document.querySelector('.yourScore').classList.remove('hide')
+   document.querySelector('.yourScore').classList.remove('hide');
+//    var myScore = localStorage.getItem("correct")
+//    var showScore = document.getElementById('score')
+//    showScore.textContent = myScore
 }
 function clockTick(){
     timeLeft--;
-    timerEl.textContent = timeLeft
+    timerEl.textContent = timeLeft;
     if(timeLeft<=0){
-        timeLeft = 0
-        timerEl.textContent = timeLeft
+        timeLeft = 0;
+        timerEl.textContent = timeLeft;
         endGame();
         viewYourScore();
     }
     
 }
 startBtn.addEventListener('click', function(){
-    document.querySelector('.start').classList.add('hide')
-    document.querySelector('.quiz').classList.remove('hide')
-    timeInterval = setInterval(clockTick, 1000)
+    document.querySelector('.start').classList.add('hide');
+    document.querySelector('.quiz').classList.remove('hide');
+    timeInterval = setInterval(clockTick, 1000);
     askQuestion();
 })
 
